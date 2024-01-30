21efffb21efafaecaa51945042ed62e22fcf11e1
Bengi Anıl <banil2@ford.com.tr>
2024-01-30 21:51:48 +0300

    Created project

---
+++ NetflixClone/NetflixCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixCloneApp.swift
+//  NetflixClone
+//
+//  Created by Bengi Anıl on 30.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct NetflixCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
