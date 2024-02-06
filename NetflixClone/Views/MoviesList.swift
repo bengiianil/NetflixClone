ec8af355a8d6917f04033e19a5ca4d95a42f1f52
Bengi Anıl <banil2@ford.com.tr>
2024-02-06 12:15:10 +0300

    Created base movie view

---
+++ NetflixClone/Views/MoviesList.swift
@@ -0,0 +1,18 @@
+//
+//  MoviesList.swift
+//  NetflixClone
+//
+//  Created by Bengi Anıl on 31.01.2024.
+//
+
+import SwiftUI
+
+struct MoviesList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MoviesList()
+}
